6a57d4d495dea9d903356a2d1fe0287ae24effad
TCL Informatix <tclbd.erp@gmail.com>
2024-03-30 12:22:30 +0600

    array

--- pullpush.py
+++ pullpush.py
@@ -1,1 +1,23 @@
 print("Hi")
+# Creating an array (list) with some elements
+my_array = [1, 2, 3, 4, 5]
+
+# Accessing elements
+first_element = my_array[0]  # Access the first element
+last_element = my_array[-1]  # Access the last element
+
+# Modifying elements
+my_array[0] = 10  # Change the first element to 10
+
+# Adding elements
+my_array.append(6)  # Add 6 to the end of the array
+
+# Removing elements
+my_array.remove(2)  # Remove the element 2 from the array
+
+# Getting the length of the array
+array_length = len(my_array)  # Returns the number of elements in the array
+
+# Iterating over the array
+for element in my_array:
+    print(element)  # Prints each element in the array
